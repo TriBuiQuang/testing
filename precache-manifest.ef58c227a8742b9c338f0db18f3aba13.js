de34a3ce357a9834ff1f0de2bf4a5aa6bde46082
Tri Bui Quang <bqdtri@tma.com.vn>
2020-03-10 15:54:05 +0700

    first commit

---
+++ precache-manifest.ef58c227a8742b9c338f0db18f3aba13.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7334a6b475c8784bd411305ad60229be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a381f7df86c0f3f200c6",
+    "url": "/static/css/main.dcf4b75a.chunk.css"
+  },
+  {
+    "revision": "ceb26275ebe904689b3e",
+    "url": "/static/js/2.d112d607.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.d112d607.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a381f7df86c0f3f200c6",
+    "url": "/static/js/main.97df1d98.chunk.js"
+  },
+  {
+    "revision": "4e3cbb152b68f6e28176",
+    "url": "/static/js/runtime-main.f107c2ba.js"
+  },
+  {
+    "revision": "421737d8f953110a2c54ee44b9eb9760",
+    "url": "/static/media/home-1-slider-image-1.421737d8.jpg"
+  },
+  {
+    "revision": "bb3cfbf53a12c1447d410dd6a83aa5f7",
+    "url": "/static/media/home-1-slider-image-2.bb3cfbf5.jpg"
+  },
+  {
+    "revision": "d7e1cfc745ebe3cfc6aa4b5c1cd07b26",
+    "url": "/static/media/home-1-slider-image-3.d7e1cfc7.jpg"
+  }
+]);
